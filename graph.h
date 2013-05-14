ef6c22e2e20509112837c0ee7473f44539a5ec66
Adomas Jatužis <adomixaszvers@gmail.com>
2013-05-14 20:12:54 +0300

    Nežinau, ką čia parašiau

--- graph.h
+++ graph.h
@@ -3,5 +3,20 @@
 
 #include <vector>
 #include <forward_list>
+#include <map>
+#include <string>
+
+struct Vertex {
+	int index;
+	int weight;
+};
+
+class Graph {
+public:
+	int& operator[] (std::string from, std::string to);
+private:
+	std::vector<std::forward_list<Vertex>> graph;
+	std::map<std::string, int> indeces;
+};
 
 #endif
